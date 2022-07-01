39f1fe817dafcdcf735f7d9404d78826e153633f
Aarya Chopkar <aaryachopkar@gmail.com>
2022-07-01 12:05:53 +0530

    Header Component

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(){
+    return(
+        <>
+        <header>
+            <h1>My Travel Journal</h1>
+        </header>
+        </>
+    )
+}
+
+export default Header;
