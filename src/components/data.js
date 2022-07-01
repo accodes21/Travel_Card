5a0c2409ec5628653f63e3fefcf2af5eacdeb135
Aarya Chopkar <aaryachopkar@gmail.com>
2022-07-01 12:06:14 +0530

    Data

---
+++ src/components/data.js
@@ -0,0 +1,35 @@
+let data = [
+    {
+        id : 1,
+        title: "Mount Fuji",
+        location: "Japan",
+        googleMapsUrl: "https://goo.gl/maps/1DGM5WrWnATgkSNB8",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://source.unsplash.com/WLxQvbMyfas"
+    },
+    {
+        id : 2,
+        title: "Sydney Opera House",
+        location: "AUSTRALIA",
+        googleMapsUrl: "https://www.google.com/maps/place/Sydney+Opera+House/@-33.8567844,151.2152967,15z/data=!4m5!3m4!1s0x0:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967",
+        startDate: "27 May, 2021",
+        endDate: "8 Jun, 2021",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+        imageUrl: "https://images.unsplash.com/photo-1527915676329-fd5ec8a12d4b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80"
+    },
+    {
+        id : 3,
+        title: "Geirangerfjord",
+        location: "NORWAY",
+        googleMapsUrl: "https://www.google.com/maps/d/viewer?ie=UTF8&t=h&oe=UTF8&msa=0&mid=1r9iHu_NaY348ww7vQhCYC8wq-UA&ll=62.09023513589082%2C7.168744081213161&z=11",
+        startDate: "01 Oct, 2021",
+        endDate: "8 Nov, 2021",
+        description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+        imageUrl: "https://images.unsplash.com/photo-1513519107127-1bed33748e4c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"
+    }
+
+]
+
+export default data;
